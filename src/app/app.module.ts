7d4de9ab97201554a56fd82f7a5e378d8109602c
vinaythakurcn <48428625+vinaythakurcn@users.noreply.github.com>
2023-01-12 14:51:37 +0530

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { OrganizationsComponent } from './organizations/organizations.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, ReactiveFormsModule, MaterialModule ],
+  declarations: [ AppComponent, HelloComponent, OrganizationsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
